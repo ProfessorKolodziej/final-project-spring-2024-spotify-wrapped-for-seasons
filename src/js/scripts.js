08b6a9f9e75c425f30f7a10ef44966b98b8feaa2
apurva-bh <apurvabh@bu.edu>
2024-04-09 20:46:01 -0400

    first round changes, almost all addition of html, basic css, basic js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,25 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+const chartCanvas = document.querySelector('#springChart');
+
+  new Chart(chartCanvas, {
+    type: 'bar',
+    data: {
+      labels: ['', '', '', '', ''],
+      datasets: [{
+        label: '# of Streams',
+        data: [12, 19, 3, 5, 2],
+        borderWidth: 1,
+        backgroundColor: ["#A94064", "#E75480", "#FF69B4", "#FFB7C5", "#FFD1DC"]
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
